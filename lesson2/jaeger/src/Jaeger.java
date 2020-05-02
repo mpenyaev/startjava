6a4ed4bce8f038da9e8ff6126373cfa07951e5c7
Mikhail Penyaev <mpenyaev@gmail.com>
2020-05-02 18:22:05 +0300

    4th folder added

---
+++ lesson2/jaeger/src/Jaeger.java
@@ -0,0 +1,63 @@
+class Jaeger{
+	private String model;
+	private String name;
+	private float height;
+	private float weight;
+	private int speed;
+
+	public Jaeger(){
+		
+	}
+	public Jaeger(String a, String b, float c, float d, int e){
+		this.model = a;
+		this.name = b;
+		this.height = c;
+		this.weight = d;
+		this.speed = e;
+	}
+
+	public void setModel(String a){
+		this.model = a;
+	}
+	public void setName(String a){
+		this.name = a;
+	}
+	public void setHeight(float a){
+		this.height = a;
+	}
+	public void setWeight(float a){
+		this.weight = a;
+	}
+	public void setSpeed(int a){
+		this.speed = a;
+	}
+	public String getModel(){
+		return model;
+	}
+	public String getName(){
+		return name;
+	}
+	public float getHeight(){
+		return height;
+	}
+	public float getWeight(){
+		return weight;
+	}
+	public int getSpeed(){
+		return speed;
+	}
+	public String toString(){
+		return "Main{"+
+		"Model='"+model+'\''+
+		", Name='"+name+'\''+
+		", Height='"+height+
+		", Weight='"+weight+
+		", Speed='"+speed+"}";
+	}
+	public void go(){
+		System.out.println("Robot "+name+" is going to ...");
+	}
+	public void go(String a){
+		System.out.println("Robot "+name+" is going to "+a);
+	}
+}
