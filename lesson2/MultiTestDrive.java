21f2068e6b514bacb21a807adbfc29363a3cbcef
Mikhail Penyaev <mpenyaev@gmail.com>
2020-05-04 11:14:49 +0300

    del the west

--- lesson2/MultiTestDrive.java
+++
@@ -1,9 +0,0 @@
-class MultiTestDrive {
-	public static void main(String[] args){
-		int p1=5;
-		int p2=7;
-		multiple life = new multiple();
-		int m = life.multiple(p1,p2);
-		System.out.println(m);
-	}
-}
